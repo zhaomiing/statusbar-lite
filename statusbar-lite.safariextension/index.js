a32b12ac539b6fb97ec97e9d69b69d4aba07e571
ZhaoMiing <zhaoming.me@gmail.com>
2015-01-15 01:17:22 +0800

    代码重构

--- statusbar-lite.safariextension/index.js
+++ statusbar-lite.safariextension/index.js
@@ -8,7 +8,7 @@
 
 'use strict';
 
-(function (win, udf) {
+(function (win, undefined) {
     var datakey = 'safaristatusbarlite',
         createEl = document.createElement.bind(document),
         createTxt = document.createTextNode.bind(document),
@@ -20,7 +20,12 @@
             timer : 200,
             txt : '此节点由safari插件自动生成'
         },
-        instance, links, /*显示(show)动画timeId*/sTid, /*隐藏(hide)动画timeId*/hTid;
+        instance,
+        saTid, //Show Animate TimeId 延时
+        haTid, //Hide Animate TimeId 延时
+        sTid,  //Show TimeId 延时
+        hTid;  //Hide TimeId 延时
+
 
     function Constructor (options) {
         this.options = options;
@@ -40,7 +45,7 @@
             document.head.appendChild(styleEl);
 
             bar.setAttribute('class', datakey + ' f-dn');
-            bar.appendChild(createTxt(this.options.txt)),
+            bar.appendChild(createTxt(this.options.txt));
             document.body.appendChild(bar);
 
             this.barEl = bar;
@@ -49,10 +54,8 @@
 
         show : function () {
             var klass = this.barEl.className,
-                reg = /\sf\-dn/ig,
-                el = this.barEl;
+                reg = /\sf\-dn/ig;
 
-            el.style.opacity = 1;            
             if( hasClass(this.barEl, 'f-dn') ){
                 this.barEl.className = klass.replace(reg, '');
             }
@@ -63,7 +66,7 @@
                 klass = this.barEl.className,
                 el = this.barEl;
 
-            el.style.opacity = 0;            
+            el.style.opacity = 0;
             if(klass.indexOf('f-dn') === -1){
                 that.barEl.className += ' f-dn';
                 that.setTxt(that.options.txt);
@@ -88,9 +91,10 @@
             var that = this,
                 el = that.barEl,
                 orOpacity = +( win.getComputedStyle(el, null).opacity ),
+                isFadeout = type === 'out',
                 isDone,
-                doLoop = function () {
-                    var isFadeout = type === 'out',
+                _doLoop = function () {
+                    var
                     // 检测动画完成？
                     _checkDone = function (curOpacity) {
                         var ret;
@@ -106,23 +110,23 @@
                     if( !isDone ) {
                         // 淡出
                         if(isFadeout) {
-                            hTid = setTimeout(function () {
+                            haTid = setTimeout(function () {
                                 that._setOpacity(orOpacity);
-                                doLoop();
+                                _doLoop();
                             }, that.options.timer / 10);
                         }
                         // 淡入
                         else{
-                            sTid = setTimeout(function () {
+                            saTid = setTimeout(function () {
                                 that._setOpacity(orOpacity);
-                                doLoop();
+                                _doLoop();
                             }, that.options.timer / 10);
                         }
                         
                     }
                     else{
-                        if(isFadeout) clearTimeout(hTid),that.hide();
-                        else clearTimeout(sTid), that.show();
+                        if(isFadeout) clearTimeout(haTid),that.hide();
+                        else clearTimeout(saTid),that.barEl.style.opacity = 1;
 
                         fn && fn.call(that);
                     }
@@ -130,7 +134,8 @@
                     isDone = _checkDone((+el.style.opacity || orOpacity));
                 };
 
-            doLoop();
+            !isFadeout && that.show();
+            _doLoop();
         },
 
         // 设置透明
@@ -144,7 +149,7 @@
     if(win.top === win) {
         instance = new Constructor(options)._init();
 
-        evProxy('body', 'a', 'mouseover', function () {
+        proxyEvent('body', 'a', 'mouseover', function () {
             var that = this,
                 tagName = that.tagName.toLowerCase(),
                 href, aNode;
@@ -155,29 +160,41 @@
             }
             // 由 a 的 childNode 触发，href 需要网上回溯到a
             else{
-                aNode = win.getClosestEl(that, 'a');
+                aNode = getClosestEl(that, 'a');
                 href = aNode.href;
             }
 
             // 考虑没有href属性的情况
             if(!/^\s*$/ig.test(href)){
                 // 跳过上轮动画
-                hTid && clearTimeout(hTid), instance.hide();
-                instance.setTxt(decodeURI(href)).fade('in');
+                haTid && clearTimeout(haTid), instance.hide();
+
+                hTid && clearTimeout(hTid);
+                sTid = setTimeout(function () {
+                    instance.setTxt(decodeURI(href)).fade('in', undefined);
+                }, 200);
             }
 
-        });
+        }, undefined);
 
-        evProxy('body', 'a', 'mouseout', function () {
-
+        proxyEvent('body', 'a', 'mouseout', function () {
+            var tid;
             // 考虑没有href属性的情况
             if(!/^\s*$/ig.test(this.href)){
                 // 跳过上轮动画
-                sTid && clearTimeout(sTid), instance.show();
-                instance.fade('out');
+                saTid && clearTimeout(saTid), instance.show();
+                
+                sTid && clearTimeout(sTid);
+                hTid = setTimeout(function () {
+                    instance.fade('out', undefined);
+                }, 200);
             }
-        });
+        }, undefined);
     }
+
+    ////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////[ utils ]/////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////
 
     /**
      * 事件代理
@@ -185,12 +202,68 @@
      * @param {string} originNode 事件元素标签名|类名|ID
      * @param {string} type 事件类型
      * @param {function} fn 事件回调函数
-     * @param {object} fn 事件回调函数参数
+     * @param {object} data 事件回调函数参数
      * @version 1.0
      * 2015-01-11
      */
-    function evProxy (superNode, originNode, type, fn, data) {
-        var sp, or, spn, spt, orn, ort, _checkFn, _fixMouseOut;
+    function proxyEvent (superNode, originNode, type, fn, data) {
+        var sp, or, spn, spt, orn, ort, _checkFn, _detectType;
+
+        //私有函数，检测evProxy参数类型, str: 元素标签名|类名|ID
+        _detectType = function (str) {
+            var ret = {},
+                type;
+
+            type = getType(str);
+
+            if(type !== 'string') {
+                ret = null;
+            }
+            else{
+                switch (str[0]){
+                    // 类名
+                    case '.':
+                        ret['dom'] = getClassEl(str.substring(1))[0];
+                        ret['type'] = 'class';
+                        break;
+                    // Id
+                    case '#':
+                        ret['dom'] = getIdEl(str.substring(1));
+                        ret['type'] = 'id';
+                        break;
+                    // 标签名
+                    default :
+                        ret['dom'] = getTagEl(str)[0];
+                        ret['type'] = 'tag';
+                        break;
+                }
+            }
+
+            return ret;
+        };
+
+        // 根据事件是否由特定原始元素触发，从而判断是否执行回调
+        _checkFn = function (e) {
+            e.preventDefault();
+
+            var tar = e.target;
+
+            if(ort === 'class') {
+                if( hasClass(tar, originNode.substring(1) ) ){
+                    fixMouseOut(e, fn, data);
+                }
+            }
+            else if(ort === 'id') {
+                if( tar.id === originNode.substring(1) ) {
+                    fixMouseOut(e, fn, data);
+                }
+            }
+            else if(ort === 'tag') {
+                if( tar.tagName.toLowerCase() === originNode || isDescendantOfTag(tar, 'a') ) {
+                    fixMouseOut(e, fn, data);
+                }
+            }
+        };
 
         sp = _detectType(superNode);
         or = _detectType(originNode);
@@ -200,180 +273,116 @@
 
         spn = sp['dom'], spt = sp['type'], orn = or['dom'], ort = or['type'];
 
-        _checkFn = function (e) {
-            e.preventDefault();
-
-            var tar = e.target;
-
-            if(ort === 'class') {
-                if( tar.hasClass( originNode.substring(1) ) ){
-                    _fixMouseOut(e, fn);
-                }
-            }
-            else if(ort === 'id') {
-                if( tar.id === originNode.substring(1) ) {
-                    _fixMouseOut(e, fn);
-                }
-            }
-            else if(ort === 'tag') {
-                // type == 'mouseover' && console.log(tar.tagName.toLowerCase());
-                if( tar.tagName.toLowerCase() === originNode || isDescendantOfTag(tar, 'a') ) {
-                    _fixMouseOut(e, fn);
-                }
-            }
-        };
-
-        /**
-         * 某元素的上级有没有某个标签
-         * @param {object} son dom
-         * @param {string} tagStr 标签名
-         * @return {boolean}
-         * @version 1.0
-         * 2015-01-11
-         */
-        function isDescendantOfTag(son, tagStr){
-            var ret = false;
-
-            if(son.tagName.toLowerCase() === tagStr) {
-                ret = true;
-            }
-            else{
-                while(son.parentNode && son.parentNode !== window) {
-                    // TODO: why try
-                    try{
-                        if(son.parentNode.tagName.toLowerCase() === tagStr) {
-                            ret = true;
-                            break;
-                        }
-                    }catch(err){}
-                
-                    son = son.parentNode;
-                }
-            }
-
-            return ret;
-        }
-
-        /**
-         * jquery.fn.closest ployfill
-         * @param {object} son dom
-         * @param {string} parentTag 上级节点标签名
-         * @return {object} ret dom
-         * @version 1.0
-         * 2015-01-11
-         */
-        function getClosestEl (son, parentTag) {
-            var ret;
-            
-            if(son.tagName.toLowerCase() === parentTag) {
-                ret = son;
-            }
-            else{
-                while(son.parentNode && son.parentNode !== window) {
-                    if(son.parentNode.tagName.toLowerCase() === parentTag) {
-                        ret = son.parentNode;
-                        break;
-                    }
-                
-                    son = son.parentNode;
-                }
-            }
-
-            return ret;
-        }
-
-        win.getClosestEl = getClosestEl;
-
-        /**
-         * mouseover 到原素childNode 上时不触发 mouseout 回调
-         * @param {object} evnet
-         * @param {function} fn 原始回调
-         * @version 1.0
-         * 2015-01-11
-         */
-        _fixMouseOut = function (event, fn) {
-            var tar = event.target,
-                reTar;
-
-            if(event.type === 'mouseout') {
-                reTar = event.relatedTarget || event.toElement;
-                
-                // reTar 只要不是 tar 的childNode 就触发
-                // reTar 是否是 a 或者 a 的 childNode
-                // 判断 reTar 是否存在，当鼠标从a元素移动到窗口外是 reTar === null
-                if ( reTar !== null && !isDescendantOrSelf(reTar, tar) ) {
-                    fn.call(tar, data);
-                }
-            }
-            else{
-                fn.call(tar, data);
-            }
-        }
-
-        /**
-         * son 是否是 father 的下级节点 或者 son本身
-         * @param {object} son dom
-         * @param {object} father dom
-         * @return {boolean}
-         * @version 1.0
-         * 2015-01-11
-         */
-        function isDescendantOrSelf(son, father) {
-            var ret = false;
-
-            if(son === father) {
-                ret = true;
-            }
-            else{
-                while(son.parentNode && son.parentNode !== window) {
-                    if(son.parentNode === father) {
-                        ret = true;
-                        break;
-                    }
-                
-                    son = son.parentNode;
-                }
-            }
-
-            return ret;
-        }
-
         spn.addEventListener(type, _checkFn, false);
     }
 
     /**
-     * 私有函数，检测evProxy参数类型
-     * @pram {string} 元素标签名|类名|ID
-     * @return {object} dom , type
+     * mouseover 到原素childNode 上时不触发 mouseout 回调
+     * @param {object} event
+     * @param {function} fn 原始回调
+     * @param {object} data fn参数
      * @version 1.0
      * 2015-01-11
      */
-    function _detectType (str) {
-        var ret = {},
-            type;
+     function fixMouseOut (event, fn, data) {
+        var tar = event.target,
+            reTar;
 
-        type = getType(str);
-        
-        if(type !== 'string') {
-            ret = null;
+        if(event.type === 'mouseout') {
+            reTar = event.relatedTarget || event.toElement;
+
+            // reTar 只要不是 tar 的childNode 就触发
+            // reTar 是否是 a 或者 a 的 childNode
+            // 判断 reTar 是否存在，当鼠标从a元素移动到窗口外是 reTar === null
+            if ( reTar !== null && !isDescendantOrSelf(reTar, tar) ) {
+                fn.call(tar, data);
+            }
         }
         else{
-            switch (str[0]){
-                // 类名
-                case '.':
-                    ret['dom'] = getClassEl(str.substring(1))[0];
-                    ret['type'] = 'class';
+            fn.call(tar, data);
+        }
+    }
+
+    /**
+     * 某元素的上级有没有某个标签
+     * @param {object} son dom
+     * @param {string} tagStr 标签名
+     * @return {boolean}
+     * @version 1.0
+     * 2015-01-11
+     */
+    function isDescendantOfTag(son, tagStr){
+        var ret = false;
+
+        if(son.tagName.toLowerCase() === tagStr) {
+            ret = true;
+        }
+        else{
+            while(son.parentNode && son.parentNode !== window) {
+                try{
+                    if(son.parentNode.tagName.toLowerCase() === tagStr) {
+                        ret = true;
+                        break;
+                    }
+                }catch(err){}
+
+                son = son.parentNode;
+            }
+        }
+
+        return ret;
+    }
+
+    /**
+     * son 是否是 father 的下级节点 或者 son本身
+     * @param {object} son dom
+     * @param {object} father dom
+     * @return {boolean}
+     * @version 1.0
+     * 2015-01-11
+     */
+    function isDescendantOrSelf(son, father) {
+        var ret = false;
+
+        if(son === father) {
+            ret = true;
+        }
+        else{
+            while(son.parentNode && son.parentNode !== window) {
+                if(son.parentNode === father) {
+                    ret = true;
                     break;
-                // Id
-                case '#':
-                    ret['dom'] = getIdEl(str.substring(1));
-                    ret['type'] = 'id';
+                }
+
+                son = son.parentNode;
+            }
+        }
+
+        return ret;
+    }
+
+    /**
+     * jquery.fn.closest ployfill
+     * @param {object} son dom
+     * @param {string} parentTag 上级节点标签名
+     * @return {object} ret dom
+     * @version 1.0
+     * 2015-01-11
+     */
+    function getClosestEl (son, parentTag) {
+        var ret;
+
+        if(son.tagName.toLowerCase() === parentTag) {
+            ret = son;
+        }
+        else{
+            while(son.parentNode && son.parentNode !== window) {
+                if(son.parentNode.tagName.toLowerCase() === parentTag) {
+                    ret = son.parentNode;
                     break;
-                // 标签名
-                default :
-                    ret['dom'] = getTagEl(str)[0];
-                    ret['type'] = 'tag';
-                    break;
+                }
+
+                son = son.parentNode;
             }
         }
 
@@ -392,9 +401,9 @@
     }
 
     /**
-     * classlist.contains polyfill
-     * @param {object} dom
-     * @param {string} className
+     * 元素是否有某个类
+     * @param {object} el dom
+     * @param {string} klass className
      * @return {boolean}
      * @version 1.0
      * 2015-01-11
